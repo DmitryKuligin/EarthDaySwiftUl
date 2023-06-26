b733dc23a9f4db5db18ae8afb28d49d42c578d2d
Дмитрий Кулигин <46721224+DmitryKuligin@users.noreply.github.com>
2023-06-26 16:08:11 +0300

    Add files via upload

---
+++ EarthDaySwiftUI/EarthDaySwiftUI/EarthDaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthDaySwiftUIApp.swift
+//  EarthDaySwiftUI
+//
+//  Created by Buddy Group
+//
+
+import SwiftUI
+
+@main
+struct EarthDaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
